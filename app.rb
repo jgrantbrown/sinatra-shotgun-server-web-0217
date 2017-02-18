e8508e309bf868866ef5dde30712c04599b49943
Your Name <you@example.com>
2017-02-18 09:28:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! THIS BAD BOYT IS ALL MINE!! WOWO really "
   end
 
 end
